ac5e4567d8f2f82f3fc89e6246a3d645e521ab2e
Vytautas Vaičys <vytautas@vaicys.com>
2019-11-05 22:02:15 +0100

    Upgrade save script to exclude undesirable packages

--- save-packages.ps1
+++ save-packages.ps1
@@ -1,3 +1,18 @@
 $packageXml = ''
-choco list -lo -r | % { $_ -split '\|' | select -first 1 } | % { $packageXml += "`n<package id=""$_"" />" }
+
+choco list -lo -r |
+    % { $_ -split '\|' | select -first 1 } |
+    Where-Object {
+        $_ -CNotMatch "\.install$" -And
+        $_ -CNotMatch "\.extension$" -And
+        $_ -CNotMatch "\.commandline$" -And
+        $_ -CNotMatch "\.portable$" -And
+        $_ -CNotMatch "^jre" -And
+        $_ -CNotMatch "^KB" -And
+        $_ -CNotMatch "^DotNet" -And
+        $_ -CNotMatch "python." -And
+        $_ -CNotMatch "vcredist[^\\-]"
+    } |
+    % { $packageXml += "`n<package id=""$_"" />" }
+
 Set-Content .\packages.config "<packages>$packageXml`n</packages>"
